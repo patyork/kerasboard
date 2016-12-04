7a70d3d675aee9d052b5b208025d7d266ce6f896
Pat York <guitarp_11@yahoo.com>
2016-12-04 15:23:59 -0800

    remove audit; temp db for testing

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ import flask_restless
 # Create the Flask application and the Flask-SQLAlchemy object.
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 
