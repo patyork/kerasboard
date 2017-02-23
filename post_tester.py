c3bad1bb90074c45b68995b9e16057ec5dc98b91
Pat York <guitarp_11@yahoo.com>
2017-02-22 16:26:54 -0800

    delay/port args

--- post_tester.py
+++ post_tester.py
@@ -1,11 +1,33 @@
 import requests
-
+import sys, getopt
 import numpy as np
 
 import time
+def usage():
+    print('''-h --help : This screen\n-p --port = Port to listen on''')
 
-id = 50
-while True:
-    r = requests.get('http://localhost:8888/api', params={'id':id, 'value':np.random.random()*1000})
-    time.sleep(.5)
-    id += 1
+if __name__ == '__main__':
+    port = 8888
+    delay = 1.0
+
+    argv = sys.argv[1:]
+    try:
+        opts, args = getopt.getopt(argv, "hpd", ["help", "port=", "delay="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-p", "--port"):
+            port = arg
+        elif opt in ("-d", "--delay"):
+            delay = arg
+
+    id = 50
+    while True:
+        r = requests.get('http://localhost:%i/api' % port, params={'id': id, 'value': np.random.random() * 1000})
+        time.sleep(delay)
+        id += 1
+
